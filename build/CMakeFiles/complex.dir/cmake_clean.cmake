d3283d62e4cbb59318aa87192400eb10eb6f2d70
Mateusz Szlachetko <mateusz.szlachetko@gmail.com>
2021-03-21 15:13:16 +0100

    folder build dla cmake,zawierający pytania do testu

---
+++ build/CMakeFiles/complex.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/complex.dir/src/Complex.cpp.o"
+  "CMakeFiles/complex.dir/src/complex_equation.cpp.o"
+  "CMakeFiles/complex.dir/src/exam.cpp.o"
+  "CMakeFiles/complex.dir/src/main.cpp.o"
+  "CMakeFiles/complex.dir/src/statistics.cpp.o"
+  "complex"
+  "complex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/complex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
